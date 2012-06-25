8bb2b1425b5dcc0c4a28d333ec13da83e921d355
byo@byone <byo@byone>
2012-06-25 23:44:39 +0200

    tried to make sense of the encoding problems

--- modules/megahal.py
+++ modules/megahal.py
@@ -9,16 +9,15 @@ import urllib2
 
 def _strip(phenny, input):
     #print "_strip: " + input
+    #i = unicode(input, errors='ignore')
     r = input.lstrip(phenny.nick)
     r = r.lstrip(": ")
-    print "Stripped: " + r
+    #print u"Stripped: " + unicode(r, errors='ignore')
     return r
 
 def learn(phenny, input):
     if input.startswith("."): return
-    #print "learn: " + input
     i = _strip(phenny, input)
-    #print "l: " + i
     input.brain.learn(i)
 
 learn.rule = r'.*'
@@ -27,14 +26,12 @@ learn.thread = False
 
 
 def reply(phenny, input):
-    #print "reply: " + input
     r = _strip(phenny, input)
     if r.startswith("reload"): return
-    #print "r: " + r
     response = input.brain.reply(r)
     phenny.say(input.nick + ": " + response)
 
-reply.rule = r'$nickname: .*'
+reply.rule = r'$nickname .*'
 reply.priority = 'high'
 reply.thread = False
 
@@ -56,7 +53,7 @@ def urllearn(phenny, input):
         headers['Cache-Control'] = 'max-age=0'
         headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
         headers['Accept-Language'] = 'sv-SE,en-US,en;q=0.8'
-        headers['Accept-Charset'] = 'utf-8,ISO-8859-1;q=0.7,*;q=0.3'
+        headers['Accept-Charset'] = 'ISO-8859-1;q=0.7,*;q=0.3'
         headers['User-Agent'] = useragent.random()
 
         redirect_handler = urllib2.HTTPRedirectHandler()
@@ -64,8 +61,8 @@ def urllearn(phenny, input):
         f = opener.open(urllib2.Request(url=url, headers=headers))
         bs = BeautifulSoup(f.read())
         for s in bs.body.strings:
-            print "Learning: " + s
-            input.brain.learn(s)
+            #print u"Learning: " + s.encode('utf-8', errors='ignore')
+            input.brain.learn(s.encode('utf-8', errors='ignore'))
 
 urllearn.rule = r'\.urllearn (\S+)'
 urllearn.priority = 'high'
